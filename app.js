a95e22814c0aa3371f0aa3b7b83dc3771395cad6
Ian <itcropper@gmail.com>
2015-07-09 14:00:36 -0600

    wrong port

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ var express = require('express'),
 app.set('view engine', 'ejs'); 
 app.set('views', __dirname + '/views');
 
+var SERVER_PORT = Number(process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
     res.render('index');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(SERVER_PORT, function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at: ', SERVER_PORT);
 
 });
